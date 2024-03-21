91bd175c5234e1feb7cca3867d1dd7547f7a420e
luciorim <88190686+luciorim@users.noreply.github.com>
2024-03-22 03:09:33 +0500

    deleted wrong services

--- school-service/src/main/java/com/luciorim/schoolservice/repository/SchoolRepository.java
+++
@@ -1,9 +0,0 @@
-package com.luciorim.schoolservice.repository;
-
-import com.luciorim.schoolservice.entities.School;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SchoolRepository extends JpaRepository<School, Long> {
-}
